04b702f6775c686e5f86db7ac257c929ce4f81db
Miguel <miguelmendozafiguera@gmail.com>
2019-04-08 09:38:36 -0400

    Start commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Responsive
+// $(document).ready(function() {
+//     if ($('.body').width() <= 1024) {
+//         console.log($('.body').width());
+//         $('#firstServ').removeClass('col-md-offset-1');
+//         $('#firstServ').removeClass('col-md-3');
+//         $('#firstServ').addClass('col-md-4');
+//     }
+// })
